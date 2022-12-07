2778e11e56d481d5f9286ca43a6e431c8dfa327a
Robert Onstott <roonstott@gmail.com>
2022-12-07 15:53:54 -0800

    Add RoundPlay(), EnterWin(), GetDictionary(), and EnterWin() methods
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>

--- RockPaperScissors.solution/RockPaperScissors.Tests/ModelTests/GameTests.cs
+++ RockPaperScissors.solution/RockPaperScissors.Tests/ModelTests/GameTests.cs
@@ -11,7 +11,7 @@ namespace RockPaperScissors.Tests
     [TestMethod]
     public void GameConstructor_CreatesInstanceOfGame_Game()
     {
-      Game newGame = new Game();
+      Game newGame = new Game("rock", "scissors");
       Assert.AreEqual(typeof(Game), newGame.GetType());
     }
 
@@ -24,7 +24,55 @@ namespace RockPaperScissors.Tests
       string result = newGame.Hand1;
       Assert.AreEqual(result, hand1);
       // Assert.AreEqual(newGame.hand2, "rock");
+    }
 
+    [TestMethod]
+    public void RoundPlay_DeterminesPlayerWon_String()
+    {
+      string hand1 = "scissors";
+      string hand2 = "paper";
+      Game newGame = new Game(hand1, hand2);
+      newGame.RoundPlay();
+      string result = newGame.Winner;
+      Assert.AreEqual("player1", result);
+    }
+
+    [TestMethod]
+
+    public void GetDictionary_ReturnsEmptyDictionary_Dictionary()
+    {
+      Dictionary<string, int> newDictionary = new Dictionary<string, int>() {{"player1", 0}, {"player2", 0}};
+      Dictionary<string, int> result = Game.GetDictionary();
+      CollectionAssert.AreEqual(newDictionary, result);
+    }
+
+    [TestMethod]
+
+    public void EnterWin_ReturnsPlayerWins_UpdatedDictionary()
+    {
+      string hand1 = "scissors";
+      string hand2 = "paper";
+      Game newGame = new Game(hand1, hand2);
+      newGame.RoundPlay();
+      newGame.EnterWin();
+      Dictionary<string, int> newDictionary = new Dictionary<string, int>() {{"player1", 1}, {"player2", 0}};
+      Dictionary<string, int> result = Game.GetDictionary();
+      CollectionAssert.AreEqual(newDictionary, result);
+    }
+
+    [TestMethod]
+    public void GetWinner_ReturnsGameWinner_String()
+    {
+      string hand1 = "scissors";
+      string hand2 = "rock";
+      Game newGame = new Game(hand1, hand2);
+      newGame.RoundPlay();
+      newGame.EnterWin();
+      string result = Game.GetWinner();
+      string compare = "no winner yet";
+      // Dictionary<string, int> newDictionary = new Dictionary<string, int>() {{"player1", 2}, {"player2", 0}};
+      // Dictionary<string, int> result = Game.GetDictionary();
+      Assert.AreEqual(compare, result);
 
     }
   }
--- RockPaperScissors.solution/RockPaperScissors/Models/Game.cs
+++ RockPaperScissors.solution/RockPaperScissors/Models/Game.cs
@@ -1,14 +1,98 @@
-// using System;
-// using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
 namespace RockPaperScissors.Models
 {
   public class Game 
   {
-    public Game()
+    public string Hand1 { get; }
+    public string Hand2 { get; }
+
+    public string Winner { get; set; }
+
+    private static Dictionary<string, int> _playerWins = new Dictionary<string, int>()
     {
-      
+      {"player1", 0},
+      {"player2", 0}
+    };
+    public Game(string hand1, string hand2)
+    {
+      Hand1 = hand1;
+      Hand2 = hand2;
+      string Winner = "tie";
+    }
+    public void RoundPlay()
+    {
+      if (Hand1 == "scissors")
+      {
+        if (Hand2 == "rock") {
+          Winner = "player2";
+        }
+        else if (Hand2 == "paper") 
+        {
+          Winner = "player1";
+        }
+        else //Hand 2 == "scissors"
+        {
+          Winner = "tie";
+        }
+      }
+      else if (Hand1 == "paper")
+      {
+        if (Hand2 == "rock") 
+        {
+          Winner = "player1";
+        }
+        else if (Hand2 == "paper") 
+        {
+          Winner = "tie";
+        }
+        else //Hand2 == "scissors"
+        {
+          Winner = "player2";
+        }
+      }
+      else //Hand1 == "rock"
+      {
+        if(Hand2 == "rock")
+        {
+          Winner = "tie";
+        }
+        else if(Hand2 == "paper")
+        {
+          Winner = "player2";
+        }
+        else //Hand2 == "scissors
+        {
+          Winner = "player1";
+        }
+      }      
     }
 
+    public void EnterWin() 
+    {
+      _playerWins[Winner] += 1;
+    }
+
+    public static Dictionary<string, int> GetDictionary()
+    {
+      return _playerWins;
+    }
+
+    public static string GetWinner()
+    {
+      if (_playerWins["player1"] == 2) 
+      {
+        return "player1";
+      }
+      else if (_playerWins["player2"] == 2)
+      {
+        return "player2";
+      }
+      else 
+      {
+        return "no winner yet";
+      }
+    }
   }
-}
+} 
